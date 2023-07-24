9b8823b990b8fbfd74e8bb69aadb7c6ea5aa2af5
Chris Holmes <chomie@gmail.com>
2023-07-24 07:40:21 -0700

    corrected email address

--- open_buildings/__init__.py
+++ open_buildings/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for open-buildings."""
 
 __author__ = """Chris Holmes"""
-__email__ = 'homie@gmail.com'
+__email__ = 'chomie@gmail.com'
 __version__ = '0.0.2'
 
 from .open_buildings import *
