580e50c08aaf28dfbdc60bd910425f7097271537
Chris Holmes <cholmes@planet.com>
2023-09-27 10:14:46 -0700

    bump version to 0.8

--- open_buildings/__init__.py
+++ open_buildings/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Chris Holmes"""
 __email__ = 'cholmes@9eo.org'
-__version__ = '0.0.7'
+__version__ = '0.0.8'
--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/opengeos/open-buildings',
-    version='0.0.7',
+    version='0.0.8',
     zip_safe=False,
 )
