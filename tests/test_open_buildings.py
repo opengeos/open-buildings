d3169c43a5d7f4ed54a524ad9a481093d97f43ed
Felix Schott <felixlschott@gmail.cm>
2023-10-12 17:07:09 +0000

    add tests

--- tests/test_open_buildings.py
+++ tests/test_open_buildings.py
@@ -1,33 +1,111 @@
-#!/usr/bin/env python
+import pytest
+from typing import Dict, Any
+from pathlib import Path
+import os
+import json
 
-"""Tests for `open_buildings` package."""
+from open_buildings.download_buildings import download, geojson_to_wkt, geojson_to_quadkey, quadkey_to_geojson
+from open_buildings.settings import Source, Format, settings
+
+NUM_RERUNS = 2 # number of re-runs for integration tests
+
+@pytest.fixture
+def aoi() -> Dict[str, Any]:
+    """ Sample AOI over Nauru. """
+    return {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          [
+            [
+              166.9108088710223,
+              -0.5298810235483273
+            ],
+            [
+              166.9108088710223,
+              -0.5305081340523401
+            ],
+            [
+              166.9114098776538,
+              -0.5305081340523401
+            ],
+            [
+              166.9114098776538,
+              -0.5298810235483273
+            ],
+            [
+              166.9108088710223,
+              -0.5298810235483273
+            ]
+          ]
+        ],
+        "type": "Polygon"
+      }
+    }
 
 
-import unittest
-from click.testing import CliRunner
+def test_geojson_to_wkt(aoi: Dict[str, Any]):
+    """ Tests the geojson_to_wkt() function. """
+    print(geojson_to_wkt(aoi))
+    assert geojson_to_wkt(aoi) == 'POLYGON ((166.9108088710223 -0.5298810235483273, 166.9108088710223 -0.5305081340523401, 166.9114098776538 -0.5305081340523401, 166.9114098776538 -0.5298810235483273, 166.9108088710223 -0.5298810235483273))'
 
-from open_buildings.google.process import process_benchmark, process_geometries
-from open_buildings import cli
+def test_geojson_to_quadkey(aoi: Dict[str, Any]):
+    """ Tests geojson_to_quadkey() using a pre-established true value. """
+    assert geojson_to_quadkey(aoi) == '311101101231'
 
+def test_quadkey_to_geojson():
+    """ Tests quadkey_to_geojson() using a pre-established true value. """
+    assert quadkey_to_geojson('031313131112') == {'type': 'Feature', 'geometry': {'type': 'Polygon', 'coordinates': [[[-0.17578125, 51.50874245880333], [-0.087890625, 51.50874245880333], [-0.087890625, 51.56341232867588], [-0.17578125, 51.56341232867588], [-0.17578125, 51.50874245880333]]]}}
 
-class TestOpen_buildings(unittest.TestCase):
-    """Tests for `open_buildings` package."""
+@pytest.mark.integration
+@pytest.mark.flaky(reruns=NUM_RERUNS)
+@pytest.mark.parametrize("source", [s for s in Source])
+def test_download(source: Source, aoi: Dict[str, Any], tmp_path: Path):
+    """ Tests that the download function successfully downloads a GeoJSON file from all sources (parametrised test) into a temporary directory (teardown after test). """
+    output_file = tmp_path.joinpath(f"output_{source.name}.json")
+    download(aoi, source=source, dst=output_file, country_iso="NR")
+    assert os.path.exists(output_file)
+    assert os.path.getsize(output_file) != 0
 
-    def setUp(self):
-        """Set up test fixtures, if any."""
+@pytest.mark.integration
+@pytest.mark.flaky(reruns=NUM_RERUNS)
+def test_download_no_output(aoi: Dict[str, Any], tmp_path: Path):
+    """ Test that no empty output file gets created if a query doesn't return anything (in this case because a wrong country_iso argument is given.) """
+    output_file = tmp_path.joinpath("no_output.json")
+    download(aoi, dst=output_file, country_iso="AI") # wrong country, aoi is in GB, not Anguilla
+    assert not os.path.exists(output_file)
 
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
+@pytest.mark.integration
+@pytest.mark.flaky(reruns=NUM_RERUNS)
+@pytest.mark.parametrize("format", [f for f in Format if f != Format.SHAPEFILE])
+def test_download_format(format: Format, aoi: Dict[str, Any], tmp_path: Path):
+    """ Requests data in all file formats defined in the settings. Attempts to validate the output for each of those too. """
+    output_file = tmp_path.joinpath(f"output.{settings.extensions[format]}")
+    download(aoi, dst=output_file, country_iso="NR")
+    assert os.path.exists(output_file)
+    assert os.path.getsize(output_file) != 0
 
-    def test_000_something(self):
-        """Test something."""
+    # validate output
+    match format:
+        case Format.GEOJSON:
+            with open(output_file, "r") as f:
+                json.load(f)
+        case Format.FLATGEOBUF:
+            pass
+        case Format.SHAPEFILE:
+            pass
+        case Format.PARQUET:
+            pass
+        case Format.GEOPACKAGE:
+            pass
+        case _:
+            raise NotImplementedError(f"Test not implemented for {format} - please add.")
 
-    # def test_command_line_interface(self):
-    #     """Test the CLI."""
-    #     runner = CliRunner()
-    #     result = runner.invoke(cli.main)
-    #     assert result.exit_code == 0
-    #     assert 'open_buildings.cli.main' in result.output
-    #     help_result = runner.invoke(cli.main, ['--help'])
-    #     assert help_result.exit_code == 0
-    #     assert '--help  Show this message and exit.' in help_result.output
+def test_download_unknwon_format(aoi: Dict[str, Any], tmp_path: Path):
+    """ Tests that an unknwon format (.abc) raises an Exception. """
+    with pytest.raises(ValueError):
+        download(aoi, dst="buildings.abc")
+
+# tests for format param (not implicit in dst)
+# tests for wrong parameters, e.g. non existing format
