059519c4fc36aaf16eda661e2e299f4aa2344f71
Felix Schott <felixlschott@gmail.cm>
2023-10-12 18:07:53 +0200

    add dockerfile, add test dependencies

---
+++ dev-container.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+#################################################################
+#                                                               #
+#   Simple Bash script to simplify development in a container.  #
+#   Builds Dockerfile.dev if it does not already exists.        #
+#   The CWD is mounted in the container.                        #
+#                                                               #
+#   Use --rebuild flag to force rebuild of the image even  if   #
+#   (a potentially older version of) the image already exists.  #
+#                                                               #
+#################################################################
+
+
+rebuild=$([[ $1 == '--rebuild' ]] && echo true || echo false)
+
+if [[ $rebuild == true ]] 
+then
+    docker build --build-arg UID=$UID --build-arg USER=$USER -t ob-dev -f Dockerfile.dev .
+else
+    docker inspect --type=image ob-dev &> /dev/null || {
+        echo "Image doesn't exist locally, building ...";
+        docker build --build-arg UID=$UID --build-arg USER=$USER -t ob-dev -f Dockerfile.dev .
+    }
+fi
+
+docker run -it --workdir /workspace -v $PWD:/workspace ob-dev bash
