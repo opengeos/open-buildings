2c074b890bb6d85dacf73bc4810e832724f02141
Felix Schott <felixlschott@gmail.cm>
2023-10-13 17:21:30 +0000

    add xdist for parallel execution of tests

--- tests/test_open_buildings.py
+++ tests/test_open_buildings.py
@@ -7,6 +7,13 @@ import json
 from open_buildings.download_buildings import download, geojson_to_wkt, geojson_to_quadkey, quadkey_to_geojson
 from open_buildings.settings import Source, Format, settings
 
+###########################################################################
+#                                                                         #
+#   RUN TESTS with `python3 -m pytest . -n <number of parallel workers>`  #
+#                                                                         #
+###########################################################################
+
+
 NUM_RERUNS = 2 # number of re-runs for integration tests
 
 @pytest.fixture
