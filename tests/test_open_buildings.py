290558a275d352812c42ff84526f40b4c0006c76
mtravis <matt.travis1@gmail.com>
2023-11-04 13:38:48 +0000

    import geocode function to tests

--- tests/test_open_buildings.py
+++ tests/test_open_buildings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import os
 import json
 
-from open_buildings.download_buildings import download, geojson_to_wkt, geojson_to_quadkey, quadkey_to_geojson
+from open_buildings.download_buildings import download, geojson_to_wkt, geojson_to_quadkey, quadkey_to_geojson, geocode
 from open_buildings.settings import Source, Format, settings
 
 ###########################################################################
